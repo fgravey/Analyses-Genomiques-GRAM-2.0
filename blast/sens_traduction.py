bb932f77c6ee29d2099d552f81870792af55327f
fgravey <fgravey@gmail.com>
2018-10-18 11:10:02 +0200

    creation fichier

---
+++ blast/sens_traduction.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+## Octobre 2018
+### Gravey François
+
+### Dictionary defintion
+trad = {"acrA" : "reverse", "acrB" : "reverse", "acrR" : "strand", \
+"ampC" : "reverse", "ampC2" : "strand", "ampD" : "strain",\
+"ampE" : "strand", "ampG" : "reverse", "ampH" : "reverse",\
+"ampR" : "strand", "dacA" : "strand", "dacB" : "strand",\
+"dacC" : "strand", "dacD" : "reverse", "fosA2" : "strand", \
+"lysR" : "reverse", "nagZ" : "strand", "ompC=ompK36" : "reverse",\
+"ompK35=ompF" : "reverse", "ompR" : "strand", "arnA" : "strand",\
+"arnB" : "strand", "arnC" : "strand", "arnd" : "strand",\
+"arnE" : "strand", "arnF" : "strand", "arnT" : "strand",\
+"phoP" : "reverse", "phoQ" : "reverse", "pmrA" : "reverse",\
+"pmrB" : "reverse", "pmrD" : "reverse"}
