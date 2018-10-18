9e5b6bf93047fa2adc9833983d7eabdcc3365523
fgravey <fgravey@gmail.com>
2018-10-18 10:26:57 +0200

    remove trad function

--- blast/ntoprot.py
+++ blast/ntoprot.py
@@ -19,46 +19,6 @@ def reversecomplement(seq):
     trans = str.maketrans("ATGC", "TACG")
     return seq.translate(trans)[::-1]
 
-##Function defintion
-def traduction(liste):
-	"""Fonction qui traduit une séquence d'ADN issue d'un gène Fasta en une
-	séquence protéique
-        Input : liste qui contient la sequence nucleotidique a a_traduire
-        Ouput : liste qui contient la sequence proteique traduite"""
-
-	prot= []
-	seq = liste
-	dico = {}
-	gencode = {
-    'ata':'i', 'atc':'i', 'att':'i', 'atg':'m',
-    'aca':'t', 'acc':'t', 'acg':'t', 'act':'t',
-    'aac':'n', 'aat':'n', 'aaa':'k', 'aag':'k',
-    'agc':'s', 'agt':'s', 'aga':'r', 'agg':'r',
-    'cta':'l', 'ctc':'l', 'ctg':'l', 'ctt':'l',
-    'cca':'p', 'ccc':'p', 'ccg':'p', 'cct':'p',
-    'cac':'h', 'cat':'h', 'caa':'q', 'cag':'q',
-    'cga':'r', 'cgc':'r', 'cgg':'r', 'cgt':'r',
-    'gta':'v', 'gtc':'v', 'gtg':'v', 'gtt':'v',
-    'gca':'a', 'gcc':'a', 'gcg':'a', 'gct':'a',
-    'gac':'d', 'gat':'d', 'gaa':'e', 'gag':'e',
-    'gga':'g', 'ggc':'g', 'ggg':'g', 'ggt':'g',
-    'tca':'s', 'tcc':'s', 'tcg':'s', 'tct':'s',
-    'ttc':'f', 'ttt':'f', 'tta':'l', 'ttg':'l',
-    'tac':'y', 'tat':'y', 'taa':'_', 'tag':'_',
-    'tgc':'c', 'tgt':'c', 'tga':'_', 'tgg':'w'}
-
-
-	debut = seq.find('atg')
-	for i in range(debut, len(seq)-2, 3):
-		mot = seq[i:i+3]
-		if mot != 'taa' and mot != 'tga' and mot != 'tag':
-			prot.append(gencode[mot])
-		else:
-			break
-
-
-	return prot
-
 def strain_trad(liste,blastdir):
 
     travail = []
