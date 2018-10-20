92f4cb37dde16120cd66c084d864dfad0806fef8
fgravey <fgravey@gmail.com>
2018-10-20 19:19:37 +0200

    analyses blastn results

--- Analyses_R/microcin/microcin.R
+++ Analyses_R/microcin/microcin.R
@@ -35,4 +35,76 @@ colnames(pc) = c("gene", "nb positifs", "Pourcentages")
 
 write.xlsx(final, file = "Microcin_results.xlsx", sheetName = "liste", row.names = FALSE, append = FALSE)
 write.xlsx(pc, file = "Microcin_results.xlsx", sheetName = "Pourcentages", row.names = FALSE, append = TRUE)
-              
+
+#############################################################################################################
+##################################### Analyses tous les E. coli même blast ##################################
+#############################################################################################################
+### Blast parameters : t = 50
+
+### Mooving to the working directory 
+setwd("~/Desktop/")
+
+### Reading the working file
+microcin = read.csv("microcin_all_fasta.csv", sep = ";", header = TRUE, stringsAsFactors = FALSE, dec = ".")
+microcin$unique = gsub(".agp.fasta", "", microcin$souche)
+microcin$unique = gsub(".awked.fasta", "", microcin$unique)
+microcin$unique = gsub(".scfd.fasta", "", microcin$unique)
+
+#### Number of tested strains :
+length(unique(microcin$unique)) ##### 422 souches
+
+#### Strain in which microcin have been find :
+microcinpos = microcin[microcin$gene != "le nombre de genes trouves est de 0",]
+microcinpos = microcinpos[!(grepl("le nombre de genes trouves est de", microcinpos$gene)),]
+
+#### Number of unique positive strains :
+length(unique(microcinpos$unique)) ##### 50 souches
+
+#### Type of gene find :
+unique(microcinpos$gene)
+
+mcmA = microcinpos[grepl("mcmA", microcinpos$gene),]
+length(unique(mcmA$unique)) #### 14 souches
+
+mcmI = microcinpos[grepl("mcmI", microcinpos$gene),]
+length(unique(mcmI$unique)) #### 14 souches
+
+mchB = microcinpos[grepl("mchB", microcinpos$gene),]
+length(unique(mchB$unique)) #### 39 souches
+
+mchI = microcinpos[grepl("mchI", microcinpos$gene),]
+length(unique(mchI$unique)) #### 39 souches
+
+mcjA = microcinpos[grepl("mcjA", microcinpos$gene),]
+length(unique(mcjA$unique)) #### 2 souches
+
+nettoyage = function(df){
+df2 = df[FALSE,]
+  for (nom in unique(df$unique)){
+    df1 = df[df$unique == nom,]
+    sup = max(as.numeric(df1$pourcentage.d.identite))
+    if (sup == 100){
+      df1$pourcentage.d.identite = gsub("100.00","100", df1$pourcentage.d.identite)
+    }
+    print(sup)
+    df1 = df1[df1$pourcentage.d.identite == sup,]
+    df2 = rbind(df2,df1)
+  }
+  return(df2)
+}
+
+mcmA_net = nettoyage(mcmA)
+mcmI_net = nettoyage(mcmI)
+mchB_net = nettoyage(mchB)
+mchB_net = mchB_net[!(grepl("mchB_Strain_Nissle2",mchB_net$gene)),]
+mchI_net = nettoyage(mchI)
+mchI_net = mchI_net[!(grepl("mchI_Strain_Nissle2",mchI_net$gene)),]
+
+
+
+write.xlsx(unique(microcinpos$unique), file = "Microcin_results.xlsx", sheetName = "liste", row.names = FALSE, append = FALSE)
+write.xlsx(mcmA_net, file = "Microcin_results.xlsx", sheetName = "mcmA", row.names = FALSE, append = TRUE)
+write.xlsx(mcmI_net, file = "Microcin_results.xlsx", sheetName = "mcmI", row.names = FALSE, append = TRUE)
+write.xlsx(mchB_net, file = "Microcin_results.xlsx", sheetName = "mchB", row.names = FALSE, append = TRUE)
+write.xlsx(mchI_net, file = "Microcin_results.xlsx", sheetName = "mchI", row.names = FALSE, append = TRUE)
+write.xlsx(mcjA, file = "Microcin_results.xlsx", sheetName = "mcjA", row.names = FALSE, append = TRUE)
