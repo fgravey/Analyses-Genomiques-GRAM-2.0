648e52af1ed5bb2213a4a9dee7cafbde34b91dcc
fgravey <fgravey@gmail.com>
2018-11-13 12:18:09 +0100

    function one ok

--- cd_hit/cd_hit_results.py
+++ cd_hit/cd_hit_results.py
@@ -2,46 +2,54 @@
 #Octobre 2018
 #Gravey François
 
-import subprocess
+# Loading modules
 import os
 import re
 from argparse import ArgumentParser
-from shutil import copyfile
 
+def parse_cd_hit(input_file):
+    #Regex definition
+    regex = re.compile("^>")
+
+    #Containers definition
+    dico = {} ## Which will contain the name of all the clusters recevered
+    resultats = [] ## Which will contain the information in the input_file
+
+    #Reading the input_file and each cluster became a key of the dico dictionnary
+    with open(input_file, 'r') as filin:
+        for ligne in filin:
+            if regex.search(ligne):
+                dico[ligne[:-1]] = []
+
+    #Reading the input_file and all the information available is put on the resultats container
+    with open(input_file, 'r') as filin:
+        for ligne in filin:
+            resultats.append(ligne[:-1])
+
+    # For each key in dico == for each cluster find by cd-hit looking for all the fasta
+    for cluster in dico.keys():
+        i = int(resultats.index(cluster)) +1
+        while regex.search(resultats[i]) == None:
+            # Mandatory in order to no be out of range
+            # For each key, all the fasta are added into the dico[key] dictionnary
+            if (int(i) < int((len(resultats)-1))) == True:
+                dico[cluster].append(resultats[i].split('\t')[1].split(",")[1].split("_")[0].replace(">",""))
+                i = i+1
+            else:
+                dico[cluster].append(resultats[i].split('\t')[1].split(",")[1].split("_")[0].replace(">",""))
+                break
+    #End of function
+    return(dico)
 
 input_file = '/Users/Francois/Documents/projets/ecloacae/patric_db/analyses/\
 blast/romr_roma/clustering/romR_clustering.fasta.clstr'
 gene = 'romR'
 outputdir = '/Users/Francois/Documents/projets/ecloacae/patric_db/analyses/\
-blast/romr_roma/'
+blast/romr_roma/clustering/'
 filename = 'romr_clustering'
 
-regex = re.compile("^>")
-dico = {}
-tout = []
-with open(input_file, 'r') as filin:
-    for ligne in filin:
-        if regex.search(ligne):
-            dico[ligne[:-1]] = []
-
-with open(input_file, 'r') as filin:
-    for ligne in filin:
-        tout.append(ligne[:-1])
-
-for cluster in dico.keys():
-    i = int(tout.index(cluster)) +1
-    while regex.search(tout[i]) == None:
-        if (int(i) < int((len(tout)-1))) == True:
-            dico[cluster].append(tout[i].split('\t')[1].split(",")[1].split("_")[0].replace(">",""))
-            i = i+1
-        else:
-            dico[cluster].append(tout[i].split('\t')[1].split(",")[1].split("_")[0].replace(">",""))
-            break
-
-with open()
-for cluster in dico.keys():
-    for fasta in dico[cluster]:
-        print("{};{}{}".format(fasta, gene,cluster.replace(">", "_")))
-#print(int(i))
-##print(len(tout)-1)
-#print(
+with open("{}{}.csv".format(outputdir, filename), 'w') as filout:
+    filout.write("Souche;{}_cd-hit_cluster\n".format(gene))
+    for cluster in parse_cd_hit(input_file).keys():
+        for fasta in parse_cd_hit(input_file)[cluster]:
+            filout.write("{};{}{}\n".format(fasta, gene,cluster.replace(">", "_")))
